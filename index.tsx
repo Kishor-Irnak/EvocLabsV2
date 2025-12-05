e78aae6b0a121336e8c64292c2aadd2a1f8b8f9f
Kishor Irnak <kishor@example.com>
2025-12-05 19:44:14 +0530

    Fixing X-axis

--- index.tsx
+++ index.tsx
@@ -1,14 +1,7 @@
-<<<<<<< HEAD
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css"; // <- ading CSS url
-=======
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import './index.css'; // <- ading CSS url
->>>>>>> 323b3986a57e5db70ed2dbd4da7d7f0b4189a577
 
 const rootElement = document.getElementById("root");
 if (!rootElement) {
