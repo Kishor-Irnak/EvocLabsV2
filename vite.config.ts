b7c23ad2ba3fdac72b15d12fc1f081645c35277f
Kishor Irnak <kishor@example.com>
2026-01-14 19:56:20 +0530

    v

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,6 @@ import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, ".", "");
-
   return {
     base: "/", // ✅ REQUIRED for custom domain
 
@@ -14,11 +12,6 @@ export default defineConfig(({ mode }) => {
     },
 
     plugins: [react()],
-
-    define: {
-      "process.env.API_KEY": JSON.stringify(env.GEMINI_API_KEY),
-      "process.env.GEMINI_API_KEY": JSON.stringify(env.GEMINI_API_KEY),
-    },
 
     resolve: {
       alias: {
